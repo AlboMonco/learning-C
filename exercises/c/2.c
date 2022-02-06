5ff9c55093d7d08a2e1b1739c0fa14a3be45689d
AlboMonco <albomonaco@gmail.com>
2022-02-05 23:40:20 -0300

    Separação por pastas

---
+++ exercises/c/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define FIBONACCI_LIMIT 4000000
+
+
+int main(){
+
+    int soma = 0;
+    int primeiro_numero = 1;
+    int segundo_numero = 2;
+    int terceiro_numero;
+
+    primeiro_numero = 3;
+    
+//     for(int i = 0; i < FIBONACCI_LIMIT; i++){
+
+//     }   
+}
