12b0e78da5c01f2e15ebbc508a75dfd6779d0930
AlboMonco <albomonaco@gmail.com>
2022-02-05 23:58:33 -0300

    Refatorando o exercicio 2

--- exercises/c/2.c
+++ exercises/c/2.c
@@ -8,6 +8,7 @@ int main(){
     int primeiro_numero = 1;
     int segundo_numero = 2;
     int terceiro_numero;
+    
     int soma = 2; 
 
     for (int i = 0; terceiro_numero < FIBONACCI_LIMIT; i++){
@@ -16,16 +17,11 @@ int main(){
 
         primeiro_numero = segundo_numero;
         segundo_numero = terceiro_numero;
+        
         if (terceiro_numero%2 == 0){
             soma+= terceiro_numero;
             printf("%d\n", soma);
         }
         
-        
-        
-
-
-    }//     for(int i = 0; i < FIBONACCI_LIMIT; i++){
-
-//     }   
+    } 
 }
