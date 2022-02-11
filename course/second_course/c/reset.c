b640cab60d2b69a6a4bc82e3b83c0421db0b33f2
AlboMonco <albomonaco@gmail.com>
2022-02-11 10:56:12 -0300

    Added test file

---
+++ course/second_course/c/reset.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char palavra_secreta[20];
+
+    sprintf(palavra_secreta, "ABACATE");
+
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    do{
+        for (int i = 0; i < strlen(palavra_secreta); i++){
+
+            int achou = 0;
+            for (int j = 0; j < tentativas; j++){
+                if (chutes[j] == palavra_secreta[i]){
+                    achou = 1;
+                    break;
+                }
+            }        
+        
+            if (achou) {
+                printf("%c ", palavra_secreta[i]);
+            } else {
+                printf("_ ");
+            }
+
+        }
+
+        char chute;
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+    } while(!acertou && !enforcou);
+
+
+}
