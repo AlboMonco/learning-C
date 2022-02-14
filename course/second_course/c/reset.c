63a1aa87bfb48a09566fd4305f03235c7b207cd9
AlboMonco <albomonaco@gmail.com>
2022-02-14 11:20:14 -0300

    File reseted to test funcionalities

--- course/second_course/c/reset.c
+++ course/second_course/c/reset.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 
-void soma(int* numero, int a, int b){
-    *numero += a+b;
+void potencia(int* resultado, int base, int expoente) {
+    int potencia = 1;
+    for (int i = 1; i <= expoente; i++){
+        potencia *= base;
+    }
+    (*resultado) = potencia;
+    
+    printf("%d\n", (*resultado));
+
 }
 
 int main(){
+    
+    int resultado;
+    int jogar = 0;
+    do{
+        int base;
+        int expoente;
+        
+        printf("Base: ");
+        scanf("%d", &base);
 
-    int num = 0;
+        printf("Expoente: ");
+        scanf("%d", &expoente);
 
-    printf("%d\n", num);
-    soma(&num, 1,5);
-    printf("%d\n", num);
+        potencia(&resultado, base, expoente);
 
-}   
+        printf("Quer jogar de novo? Sim (1) Não (0)\n");
+        scanf("%d", &jogar);   
+    
+    } while(jogar);
+
+
+   
+}
