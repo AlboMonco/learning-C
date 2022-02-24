f75d1867b24835d0844b27e8d1562de45aaaf6ea
AlboMonco <albomonaco@gmail.com>
2022-02-24 16:38:05 -0300

    Refactored and added new functions

--- course/second_course/c/forca.h
+++ course/second_course/c/forca.h
@@ -1,3 +1,5 @@
+#define TAMANHO_PALAVRA 20
+
 void abertura();
 void chuta();
 int ja_chutou(char letra);
@@ -5,3 +7,5 @@ void desenha_forca();
 void escolhe_palavra();
 int acertou();
 int enforcou();
+void adiciona_palavra();
+int chutes_errados();
