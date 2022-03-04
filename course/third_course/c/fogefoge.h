3926a06596e9ba9396779e17d4fa8dfeaf54473b
AlboMonco <albomonaco@gmail.com>
2022-03-04 13:27:57 -0300

    Header file updated with new functions

--- course/third_course/c/fogefoge.h
+++ course/third_course/c/fogefoge.h
@@ -1,3 +1,11 @@
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+
 int acabou();
 void move(char direcao);
-
+int eh_direcao(char direcao);
+void fantasmas();
+int pra_onde_o_fantasma_vai(int x_atual, int y_atual,
+    int* x_destino, int* y_destino);
