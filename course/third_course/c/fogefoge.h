1d6340cb0382fca61929e72989be9159a24a4362
AlboMonco <albomonaco@gmail.com>
2022-03-07 15:56:38 -0300

    Added compiler directive and new header file

--- course/third_course/c/fogefoge.h
+++ course/third_course/c/fogefoge.h
@@ -1,3 +1,6 @@
+#ifndef _FOGE_FOGE_H_
+#define _FOGE_FOGE_H_
+
 #define CIMA 'w'
 #define BAIXO 's'
 #define DIREITA 'd'
@@ -11,3 +14,5 @@ int pra_onde_o_fantasma_vai(int x_atual, int y_atual,
     int* x_destino, int* y_destino);
 void explode_pilula();
 void explode_pilula2(int x, int y, int soma_x, int soma_y, int quantidade);
+
+#endif
